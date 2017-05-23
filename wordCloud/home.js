167decfaefd7d6c8e29ad6bac8fcbf3657aeb8b3
Tim <490003773@qq.com>
2017-05-23 14:31:03 +0800

    Add api doc

--- wordCloud/home.js
+++ wordCloud/home.js
@@ -3,6 +3,7 @@ var arr = ["红楼梦", "三国演义", "西游记", "梦三国", "LOL", "疯狂
   "傻老头", "鸳鸯", "贾雨村", "王夫人", "晴雯", "亚里士多德", "Michelle", "John", "Tim",
   "小红", "小明", "李雷", "韩梅梅", "三十六计", "曹冲", "走秀", "香菱"
 ];
+console.log(arr.length);
 var list = [], shapes = ["circle", "diamond", "triangle-foward", "cardioid", "triangle", "pentagon", "star"];
 for (var i = 0, len = arr.length; i < len; i++) {
   list.push([arr[i], Math.floor(Math.random() * 100)]);
